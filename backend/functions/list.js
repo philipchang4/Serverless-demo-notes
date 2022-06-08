411f8429136899c3b17774e269ad6c1eddf003db
Philip Chang <chan987@usc.edu>
2022-06-08 14:39:06 -0700

    Setting up frontend react application

--- backend/functions/list.js
+++ backend/functions/list.js
@@ -11,7 +11,7 @@ export const main = handler(async (event) => {
     // 'ExpressionAttributeValues' defines the value in the condition
     // - ':userId': defines 'userId' to be the id of the author
     ExpressionAttributeValues: {
-      "userId:" event.requestContext.authorizer.iam.cognitoIdentity.identityId,
+      "userId": event.requestContext.authorizer.iam.cognitoIdentity.identityId,
     },
   };
 
